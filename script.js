0a96541886b823f10911fd8707a3addda178ac8a
Christopher Perry <c.perry@icloud.com>
2018-12-26 16:46:14 -0700

    test

--- script.js
+++ script.js
@@ -4,14 +4,11 @@ nameBirthDay.onreadystatechange = function() {
       let stepOne = JSON.parse(this.responseText);
       document.getElementById("name").innerHTML = stepOne.fullName;
       document.getElementById("bDay").innerHTML = stepOne.birthDay;
-
     }};
-
     nameBirthDay.open("GET", "einstein.json", true);
     nameBirthDay.send();
  
-
-    function loadBio(){
+        function loadBio(){
         let newBio = new XMLHttpRequest();
         newBio.onreadystatechange = function() {
           if (this.readyState == 4 && this.status == 200) {
@@ -19,7 +16,6 @@ nameBirthDay.onreadystatechange = function() {
                 document.getElementById("theBio").innerHTML = stepTwo.entireBio;
           }};
           newBio.open("GET", "einstein.json", true);
-          newBio.send(); 
-          
+          newBio.send();    
         };
       
